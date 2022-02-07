d802b86c245849ff92f182949c4d43547ea2a0b6
zuhair <zuhair@example.com>
2022-02-07 18:29:25 -0500

    Version 1

---
+++ rock-paper-scissors.js
@@ -0,0 +1,12 @@
+/*Pseudocode*/
+    /*Declare a variable with the name "Rock"*/
+    /*Declare a variable with the name "Paper"*/
+    /*Declare a variable with the name "Scissors"*/
+    /*Declare that Rock beats Scissors*/
+    /*Declare that Paper beats Rock*/
+        /*if paper beats rock, a string returns saying "You Lose! Paper beats Rock"*/
+        /*Declare that the playerSelection is caseinsensitive, meaning they can return any value selected*/
+    /*Declare that Scissors beats Paper*/
+    /*Declare a function that with two perameters, playerSelection and computerSelection*/
+    /*Create a function that allows the computer and player to play against one another, assigning the variables randomly to player and computer*/
+    /*Create a function that plays the game Rock, Paper, Scissors for one round*/
